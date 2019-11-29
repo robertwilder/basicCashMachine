0c3bdafdda3a79a2ec132a70fe39cb3d2afd5420
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-29 12:07:51 +0000

    Second file, use of pin

---
+++ cashmachine2.js
@@ -0,0 +1,12 @@
+let pin = 1234;
+let Balence = 1000
+let withdrawlAmount = 1001
+const security = () => {
+    if (pin == 1234){
+        console.log("Access Granted")
+    }
+    else {
+        console.log("Access Denied")
+    }
+}
+security()
